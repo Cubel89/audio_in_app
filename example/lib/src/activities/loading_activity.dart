4780da4a1ee2182f91a473d89d2c3bb83931ad30
Cubel <p.cubel@gmail.com>
2023-10-24 16:24:39 +0200

    errores

--- example/lib/src/activities/loading_activity.dart
+++ example/lib/src/activities/loading_activity.dart
@@ -29,7 +29,7 @@ class _LoadingActivityState extends State<LoadingActivity> {
   Future<void> goToMain() async {
     //_audioInApp.initialize();
     await _audioInApp.createNewAudioCache(playerId: 'button', route: 'assets/audio/button.wav', audioInAppType: AudioInAppType.determined);
-    await _audioInApp.createNewAudioCache(playerId: 'intro1', route: 'audio/intro_1.wav', audioInAppType: AudioInAppType.background);
+    await _audioInApp.createNewAudioCache(playerId: 'intro1', route: 'assets/audio/intro_1.wav', audioInAppType: AudioInAppType.background);
     //await _audioInApp.createNewAudioCache(playerId: 'intro2', route: 'audio/intro_2.wav', audioInAppType: AudioInAppType.background);
     //log(_audioInApp._audioPlayerMap.toString(), name: 'LoadingActivity');
 
